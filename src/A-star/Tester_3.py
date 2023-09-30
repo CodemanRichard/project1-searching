d654a9a811ce7ac3d596bbf127b7a7b045309f86
ztr <1621456391@sjtu.edu.cn>
2023-09-30 16:55:18 +0800

    tools_3 and Tester_3 updated

--- src/A-star/Tester_3.py
+++ src/A-star/Tester_3.py
@@ -1,62 +1,52 @@
 from tools_2 import AStarSolver_2
+from tools_3 import AStarSolver_3
 from time import time
 from queue import PriorityQueue
 
 class Tester:
     def __init__(self):
-        self.target_seq_1 = None
-        self.target_seq_2 = None
-        self.target_solution_1 = None
-        self.target_solution_2 = None
-        self.target_match_cost_1 = float('inf')
-        self.target_match_cost_2 = float('inf')
+        self.target_seq = None
+        self.target_solution = None
+        self.target_match_cost = float('inf')
         self.matchSeq = str(input('Please input the sequence to match: '))
         databaseName = str(input('Please input the file name of database: '))
         with open(databaseName, 'r') as dataFile:
             self.dataSeq = dataFile.read().splitlines()
     
     def run_test(self):
-        # 要存前两个最大的，使用 PriorityQueue
         target_index = 0
         begin_time = time()
-        solver_array = list(range(len(self.dataSeq)))
-        cost_of_all = PriorityQueue()
+        dataPairSeq = []
+        for (index, seq1) in enumerate(self.dataSeq):
+            for seq2 in self.dataSeq[(index+1):]:
+                dataPairSeq.append((self.matchSeq, seq1, seq2))
+        solver_array = list(range(len(dataPairSeq)))
 
-        for (index, seq) in enumerate(self.dataSeq):
-            solver_array[index] = AStarSolver_2((self.matchSeq, seq))
-            match_cost = solver_array[index].solve()
-            cost_of_all.put((match_cost, index))
-
-        biggest_match = cost_of_all.get()
-        target_index = biggest_match[1]
-        self.target_seq_1 = self.dataSeq[target_index]
-        self.target_solution_1 = solver_array[target_index].align_seq()
-        self.target_match_cost_1 = biggest_match[0]
-
-        second_biggest_match = cost_of_all.get()
-        target_index = second_biggest_match[1]
-        self.target_seq_2 = self.dataSeq[target_index]
-        self.target_solution_2 = solver_array[target_index].align_seq()
-        self.target_match_cost_2 = second_biggest_match[0]
-
+        for (index, seq) in enumerate(dataPairSeq):
+            # 使用下界估计判断是否需要进行匹配
+            lower_bound = (AStarSolver_2((seq[0], seq[1])).solve()+
+                           AStarSolver_2((seq[0], seq[2])).solve()+
+                           AStarSolver_2((seq[1], seq[2])).solve())
+            if lower_bound < self.target_match_cost:
+                solver_array[index] = AStarSolver_3(seq)
+                match_cost = solver_array[index].solve()
+                if match_cost < self.target_match_cost:
+                    target_index = index
+                    self.target_match_cost = match_cost
+        self.target_seq = dataPairSeq[target_index]
+        self.target_solution = solver_array[target_index].align_seq()
         end_time = time()
         time_used = end_time - begin_time
 
         print('\n')
-        print('The sequence that matches the input sequence most is:')
-        print(self.target_seq_1)
-        print('The two sequences after matching:')
-        print(self.target_solution_1[0])
-        print(self.target_solution_1[1])
-        print(f'The cost of matching is: {self.target_match_cost_1}')
-
-        print('\n')
-        print('The sequence that matches the input sequence second most is:')
-        print(self.target_seq_2)
-        print('The two sequences after matching:')
-        print(self.target_solution_2[0])
-        print(self.target_solution_2[1])
-        print(f'The cost of matching is: {self.target_match_cost_2}')
+        print('The sequences that match the input sequence most are:')
+        print(self.target_seq[1])
+        print(self.target_seq[2])
+        print('The three sequences after matching:')
+        print(self.target_solution[0])
+        print(self.target_solution[1])
+        print(self.target_solution[2])
+        print(f'The cost of matching is: {self.target_match_cost}')
 
         print('\n')
         print(f'Total time used: {time_used}')
---
+++ src/A-star/tools_3.py
@@ -0,0 +1,149 @@
+from queue import PriorityQueue
+import numpy as np
+
+MISMATCH_COST = 5
+GAP_COST = 3
+
+
+class AStarSolver_3:
+    def __init__(self, seqs):
+        self.seqs = seqs
+        self.visited = np.zeros(shape=(len(seqs[0]), len(seqs[1]), len(seqs[2])), dtype=bool)
+        self.queue = PriorityQueue()
+        self.init_node = AStarNode(0, (0,0,0), None, self)
+        self.end_node = None
+        self.match_cost = 0
+
+    def solve(self):
+        """Find the minimum match cost between three sequences"""
+        self.queue.put(self.init_node)
+        while True:
+            cur_node = self.queue.get()
+            if cur_node.is_target():
+                self.end_node = cur_node
+                self.match_cost = cur_node.cost
+                return self.match_cost
+            else:
+                for node in cur_node.expand():
+                    self.queue.put(node)
+
+    def align_seq(self):
+        """Return the aligned sequences of three input sequences"""
+        seq1 = ""
+        seq2 = ""
+        seq3 = ""
+        cur_node = self.end_node
+        next_pos = None
+        cur_pos = None
+        while True:
+            next_pos = cur_node.parent.pos
+            cur_pos = cur_node.pos
+            if cur_pos[0]==next_pos[0]:
+                seq1 += '*'
+            else:
+                seq1 += self.seqs[0][cur_pos[0]]
+            if cur_pos[1]==next_pos[1]:
+                seq2 += '*'
+            else:
+                seq2 += self.seqs[1][cur_pos[1]]
+            if cur_pos[2]==next_pos[2]:
+                seq3 += '*'
+            else:
+                seq3 += self.seqs[2][cur_pos[2]]
+            cur_node = cur_node.parent
+            if cur_node.pos==(0,0,0):
+                break
+        return (self.seqs[0][0]+seq1[::-1], self.seqs[1][0]+seq2[::-1], self.seqs[2][0]+seq3[::-1])
+
+
+    
+class AStarNode:
+    def __init__(self, cost, pos, parent, solver):
+        """Initialization of an A* node"""
+        self.cost = cost
+        self.pos = pos
+        self.parent = parent
+        self.solver = solver
+        self.heuristic_cost = self.heuristic()
+        self.evaluated_cost = self.cost + self.heuristic_cost
+
+    def __lt__(self, other):
+        """Comparison between two A* nodes"""
+        self.evaluated_cost = self.cost + self.heuristic()
+        other.evaluated_cost = other.cost + other.heuristic()
+        return self.evaluated_cost < other.evaluated_cost
+
+    # need to implement another heuristic function
+    def heuristic(self):
+        """The heuristic function of A*"""
+        # h(n)定义为两条序列剩下部分长度之差乘上匹配空隙的代价
+        # 对于三序列匹配，进行两两比对
+        len_1 = len(self.solver.seqs[0]) - (self.pos[0]+1)
+        len_2 = len(self.solver.seqs[1]) - (self.pos[1]+1)
+        len_3 = len(self.solver.seqs[2]) - (self.pos[2]+1)
+        return GAP_COST * (abs(len_1-len_2) + abs(len_1-len_3) + abs(len_2-len_3))
+
+    def is_target(self):
+        """Return if the node has reached the terminate condition"""
+        return (self.pos[0]==len(self.solver.seqs[0])-1 
+                and self.pos[1]==len(self.solver.seqs[1])-1
+                and self.pos[2]==len(self.solver.seqs[2])-1)
+
+    def Node(self, cost, pos):
+        """Return a child node of the current node"""
+        return AStarNode(cost, pos, self, self.solver)
+
+    def expand(self):
+        """Return all nodes that can be expanded to"""
+        if self.solver.visited[self.pos]:
+            return []
+        else:
+            self.solver.visited[self.pos] = True
+            expand_nodes = []
+            # (x,y,z)->(x+1,y,z)
+            if self.pos[0] < len(self.solver.seqs[0])-1:
+                next_pos = (self.pos[0]+1, self.pos[1], self.pos[2])
+                expand_nodes.append(self.Node(self.cost+2*GAP_COST, next_pos))
+            # (x,y,z)->(x,y+1,z)
+            if self.pos[1] < len(self.solver.seqs[1])-1:
+                next_pos = (self.pos[0], self.pos[1]+1, self.pos[2])
+                expand_nodes.append(self.Node(self.cost+2*GAP_COST, next_pos))
+            # (x,y,z)->(x,y,z+1)
+            if self.pos[2] < len(self.solver.seqs[2])-1:
+                next_pos = (self.pos[0], self.pos[1], self.pos[2]+1)
+                expand_nodes.append(self.Node(self.cost+2*GAP_COST, next_pos))
+            # (x,y,z)->(x+1,y+1,z)
+            if self.pos[0] < len(self.solver.seqs[0])-1 and self.pos[1] < len(self.solver.seqs[1])-1:
+                next_pos = (self.pos[0]+1, self.pos[1]+1, self.pos[2])
+                if self.solver.seqs[0][self.pos[0]] == self.solver.seqs[1][self.pos[1]]:
+                    expand_nodes.append(self.Node(self.cost+2*GAP_COST, next_pos))
+                else:
+                    expand_nodes.append(self.Node(self.cost+MISMATCH_COST+2*GAP_COST, next_pos))
+            # (x,y,z)->(x+1,y,z+1)
+            if self.pos[0] < len(self.solver.seqs[0])-1 and self.pos[2] < len(self.solver.seqs[2])-1:
+                next_pos = (self.pos[0]+1, self.pos[1], self.pos[2]+1)
+                if self.solver.seqs[0][self.pos[0]] == self.solver.seqs[2][self.pos[2]]:
+                    expand_nodes.append(self.Node(self.cost+2*GAP_COST, next_pos))
+                else:
+                    expand_nodes.append(self.Node(self.cost+MISMATCH_COST+2*GAP_COST, next_pos))
+            # (x,y,z)->(x,y+1,z+1)
+            if self.pos[1] < len(self.solver.seqs[1])-1 and self.pos[2] < len(self.solver.seqs[2])-1:
+                next_pos = (self.pos[0], self.pos[1]+1, self.pos[2]+1)
+                if self.solver.seqs[1][self.pos[1]] == self.solver.seqs[2][self.pos[2]]:
+                    expand_nodes.append(self.Node(self.cost+2*GAP_COST, next_pos))
+                else:
+                    expand_nodes.append(self.Node(self.cost+MISMATCH_COST+2*GAP_COST, next_pos))
+            # (x,y,z)->(x+1,y+1,z+1)
+            if (self.pos[0] < len(self.solver.seqs[0])-1
+                and self.pos[1] < len(self.solver.seqs[1])-1
+                and self.pos[2] < len(self.solver.seqs[2])-1):
+                next_pos = (self.pos[0]+1, self.pos[1]+1, self.pos[2]+1)
+                mismatch_count = 0
+                if self.solver.seqs[0][self.pos[0]] != self.solver.seqs[1][self.pos[1]]:
+                    mismatch_count += 1
+                if self.solver.seqs[1][self.pos[1]] != self.solver.seqs[2][self.pos[2]]:
+                    mismatch_count += 1
+                if self.solver.seqs[0][self.pos[0]] != self.solver.seqs[2][self.pos[2]]:
+                    mismatch_count += 1
+                expand_nodes.append(self.Node(self.cost+mismatch_count*MISMATCH_COST, next_pos))
+            return expand_nodes
